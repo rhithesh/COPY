63e2cc7c96805f711c1c66d27fef8910b35da1f2
rhithesh <rhithesh1947@gmail.com>
2024-01-24 02:05:59 +0530

    deploy issue

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 import { Button } from "@/components/Button";
 import Link from "next/link";
 
-const socket = io("https://copy-2vlc-4y32cy5l9-rhithesh.vercel.app", {
+const socket = io("https://copy-rouge.vercel.app", {
 	transports: ["websocket"],
 });
 
