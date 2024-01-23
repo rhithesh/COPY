20da848e78c6492d1339e189cb81b92bc146f601
rhithesh <rhithesh1947@gmail.com>
2024-01-24 02:06:23 +0530

    deploy issue

--- app/socket.ts
+++ app/socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-export const socket = io("https://copy-rouge.vercel.app/", {
+export const socket = io("https://copy-rouge.vercel.app", {
 	transports: ["websocket"],
 });
